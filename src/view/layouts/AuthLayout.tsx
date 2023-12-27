0dec264c4c564dbe9c59869c9b01dd4b64a8d70e
lucasellery <olucasellery@gmai.com>
2023-12-26 21:37:43 -0300

    feat: adding login page

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,10 +1,32 @@
+import illustration from '../../assets/images/illustration.png';
+import { Logo } from '../components/Logo';
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full flex items-center justify-center flex-col gap-16">
+        <Logo className='text-gray-500 h-6' />
+
+        <div className="mt-16 w-full max-w-md">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="w-1/2 h-full  justify-center items-center p-8 hidden">
+        <img
+          src={illustration}
+          alt=""
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="absolute max-w-[656px] bottom-8 bg-white p-10 rounded-b-[32px]">
+          <Logo className='text-teal-900 h-8' />
+          <p className='text-gray-700 font-medium text-xl mt-6'>
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
